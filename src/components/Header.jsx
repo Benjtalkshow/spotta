63f7c6d95a5f0f16b40c01095046b09a592c29df
Benjtalkshow <chinedubenj@gmail.com>
2024-04-05 06:13:13 -0700

    Feat: Implement Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <div className="w-full py-5 px-5 md:px-20">
+      <header className="flex items-center justify-between">
+        {/* logo */}
+        <div className="logo flex gap-2 items-center">
+          <h1 className="font-bold text-xl sm:text-3xl">SPOTTA</h1>
+          <img
+            src={logo}
+            alt="logo"
+            className="w-min sm:w-[50px] h-min sm:h-[50px]"
+          />
+        </div>
+        {/* link or profile */}
+        <div className="font-bold text-[#5378F6]">
+          {isLoggedIn ? <h1>USER</h1> : <Link to="/login">LOGIN</Link>}
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from '../components'
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-[#FAFCFD]'>
+        <Header />
+    </div>
+  )
+}
+
+export default Home
